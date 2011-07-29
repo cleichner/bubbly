41fbf775a5239eb9dfbe7d4478940f419d41317a
Chas <chaoslichen@gmail.com>
2011-07-29 01:55:25 -0700

    updated movements to contain intializer and cleaned up comments

--- movements.h
+++ movements.h
@@ -9,18 +9,20 @@ typedef enum{
     RIGHT,
 } direction_t;
 
-/* moves the robot one square forward*/
+// Any initialization that needs to happen in the main function.
+void initialize_movements(void);
+
+// Move one square forward.
 void move_forward(void);
 
-/* rotate to the 90 degrees counter-clockwise */
+// Rotate to the 90 degrees counter-clockwise.
 void rotate_left(void);
 
-/* rotate to the 90 degrees clockwise */
+// Rotate to the 90 degrees clockwise.
 void rotate_right(void);
 
-/* respond if there is currently a wall in the specified direction with 
- * respect to the robot
- */
+// Returns true if there is currently a wall in the specified direction with
+// respect to the robot.
 bool has_wall(direction_t direction);
 
-#endif
+#endif // MOVEMENT_H

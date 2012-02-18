5e5cb7397301522cda2a118866bcb9595089db90
Kevin Klug <Kevin@KevinKlug.org>
2012-02-17 21:13:59 -0700

    Fixed ISR init

--- hardware/physical/encoder.c
+++ hardware/physical/encoder.c
@@ -1,6 +1,6 @@
 //***************************************************************//
 //
-//File: main.c
+//File: encoder.c
 //Author: Kevin Klug
 //Date: 2/14/2012
 //
@@ -18,48 +18,45 @@
 #include <avr/interrupt.h>
 #include <stdint.h>
 
-//*****************//
-//Digital ISR
-//*****************//
-ISR (PCINT2_vect) {
-    //This interrupt will be called when PCINT(16,17,21,22) are changed
-    if ((PORTD & PinD0) != (current_state & PinD0))
-        current_state ^= PinD0;
-    if ((PORTD & PinD1) != (current_state & PinD1))
-        current_state ^= PinD1;
-    if ((PORTD & PinD5) != (current_state & PinD5))
-        current_state ^= PinD5;
-    if ((PORTD & PinD6) != (current_state & PinD6)) 
-        current_state ^= PinD6;    
-    EUF++;
-}
+
 
 //Configures the ATMEGA registers for correct usage of the
 //PORTD interrupts.
 void enc_init(void) {
-    //Set Encoder pins as inputs
-    DDRD = DDRD & 0x1C;
-    //Write Logic LOW to pins (HIGH-Z)
-    PORTD = PORTD & 0x1C;
     
-    //Enable Interrupt to PCIN2_vect for PCINT16,17,21,22    
-    //Mask all others
-    PCMSK2 = 0x63;
-    PCICR = 0x04;
+    //Enable Serial Debugging
+    //Note: This means only the right motor can be debugged as the left one
+    //      uses PD0(RX) and PD1(TX)
+    //Serial.begin(9600);
+    //Serial.println("Initalizing Serial");
+    
+    //Configure Pins PD5 & PD6 as inputs by setting (0)
+    DDRD &= ~(1<<5);
+    DDRD &= ~(1<<6);
+    
+    //Enable Interrupt Procedure
+    PCMSK2 |= (1<<PCINT21);
+    //A logic change on PCINT21 will enable interrupt to PCINT2_vect
+    PCMSK2 |= (1<<PCINT22);
+    PCICR |= (1<<PCIE2);
+    
+    //Enable all interrupts
+    sei();
     
 }
 
-void update_encoder(void) {
+
+void update_encoder (void){
     //Reset flag
     EUF = 0;
-    
+    cli();
     //Now find out which motors changed and in which direction
     //using the following coding
     
     //Encoding Table
     //__________________
     //Clockwise Rotation
-    //Phase    A    B   Return
+    //Phase A    B   Return
     //1     0    0   0
     //2     0    1   1
     //3     1    1   3
@@ -70,79 +67,41 @@ void update_encoder(void) {
     //1     1    0   2
     //2     1    1   3
     //3     0    1   1
-    //4     0    0   0
-    
-    //Logic Table for left motor
-    switch (left_current_state()) {
+    //4     0    0   0  
+    switch(right_current_A){
         case 0:
-            if (left_past_state() == 2) {
-                //CW rotation
-                l_state++;
-            } else {
-                //CCW Rotation
-                l_state--;
-            }break;
+            if(!right_past_B) right_turns++;
+            else right_turns--;
+            break;
             
         case 1:
-            if (left_past_state() == 0) {
-                l_state++;
-            } else {
-                l_state--;
-            }break;
-            
-        case 2:
-            if (left_past_state() == 3){
-                l_state++;
-            } else {
-                l_state--;
-            }break;
-            
-        case 3:
-            if (left_past_state() == 1) {
-                l_state++;
-            } else {
-                l_state--;
-            }break;
+            if(right_past_B) right_turns++;
+            else right_turns--;
+            break;
             
         default:
             break;
     }
+    //Update the past
+    right_past_A = right_current_A;
+    right_past_B = right_current_B;
     
-    //Logic Table for right motor
-    switch (right_current_state()) {
+    switch(left_current_A){
         case 0:
-            if (right_past_state() == 2) {
-                //CW rotation
-                r_state++;
-            } else {
-                //CCW Rotation
-                r_state--;
-            }break;
-        case 1:
-            if (right_past_state() == 0) {
-                r_state++;
-            } else {
-                r_state--;
-            }break;
-        case 2:
-            if (right_past_state() == 3){
-                r_state++;
-            } else {
-                r_state--;
-            }break;
+            if(!left_past_B) left_turns++;
+            else left_turns--;
+            break;
             
-        case 3:
-            if (right_past_state() == 1) {
-                r_state++;
-            } else {
-                r_state--;
-            } break;
+        case 1:
+            if(left_past_B) left_turns++;
+            else left_turns--;
+            break;
             
         default:
             break;
     }
+    left_past_A = left_current_A;
+    left_past_B = left_current_B;
+    sei();
     
-    EUF = 0;
 }
-
-
